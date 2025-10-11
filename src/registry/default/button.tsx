4942cf267031e9daee6c65abd49cf2e8dd33ebf2
deepu7d <deepanshu.9dd@gmail.com>
2025-10-11 18:30:45 +0530

    add navbar

--- src/registry/default/button.tsx
+++
@@ -1,3 +0,0 @@
-export default function Button() {
-  return <button>I am button</button>;
-}
---
+++ src/registry/default/navbar.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { useState } from "react";
+import { motion } from "motion/react";
+import Link from "next/link";
+
+type NavItem = {
+  name: string;
+  href: string;
+};
+
+const navItems: NavItem[] = [
+  { name: "Home", href: "/" },
+  { name: "About", href: "/about" },
+  { name: "Services", href: "/services" },
+  { name: "Contact", href: "/contact" },
+] as const;
+
+export default function FramerMotionNav() {
+  const [hoveredTab, setHoveredTab] = useState<string | null>(null);
+
+  return (
+    <nav
+      className="relative inline-flex items-center bg-dark p-2 rounded-lg"
+      onMouseLeave={() => setHoveredTab(null)}
+    >
+      {navItems.map((item) => (
+        <Link key={item.name} href={"#"} className="mx-1">
+          <button
+            onMouseEnter={() => setHoveredTab(item.name)}
+            className={`relative rounded-md px-3 py-2 text-sm font-medium transition-colors duration-300`}
+          >
+            <span className="relative z-10">{item.name}</span>
+
+            {hoveredTab === item.name && (
+              <motion.div
+                layoutId="active-pill"
+                transition={{ type: "spring", duration: 0.3 }}
+                className="absolute inset-0 bg-light rounded-md z-0"
+              />
+            )}
+          </button>
+        </Link>
+      ))}
+    </nav>
+  );
+}
